537b05755498f6fca8c2b38508f7ae23d248bd0f
tobi <96433729+meowfluff@users.noreply.github.com>
2022-03-03 18:45:42 +0000

    Update ProjectTiles.js

--- js/ProjectTiles.js
+++ js/ProjectTiles.js
@@ -19,7 +19,8 @@ class Tile {
 }
 
 _LISTS = [
-    new List("Unreleased / In Development", "list_unreleased"),
+    new List("Incomplete / In Development", "list_unreleased"),
+    new List("March 2022", "list_2022_march"),
     new List("February 2022", "list_2022_february"),
     new List("January 2022", "list_2022_january"),
     new List("December 2021", "list_2021_december")
@@ -27,9 +28,11 @@ _LISTS = [
 
 _PROJECTS = [
     // in development
-    new Tile("JesusAI", "a batch game involving Jesus, debugging features and 4 endings", "co-developing, recoding parts of the game", "https://github.com/JesusAIexperience/JesusAI", "web_fpr", "list_unreleased", true, true),
-    new Tile("JesusAI Cartridges", "optional DLC that can run from JesusAI", "coding", "https://github.com/meowfluff/JesusAI-Cartridges", "web_fpr", "list_unreleased", true, true),
-    new Tile("Auxilium", "A 2-player experience about teamwork and betrayal", "coding", "https://github.com/meowfluff/Auxilium", "web_fpr", "list_unreleased", true, true),
+    new Tile("Auxilium", "A two player or singleplayer experience about teamwork and betrayal", "coding", "https://github.com/meowfluff/Auxilium", "web_fpr", "list_unreleased", true, true),
+    
+    // march 2022
+    new Tile("JesusAI", "a batch game involving Jesus, debugging features and 4 endings", "co-developing, recoding parts of the game", "https://github.com/JesusAIexperience/JesusAI", "web_fpr", "list_2022_march", true, true),
+    new Tile("JesusAI Cartridges", "optional DLC that can run from JesusAI", "coding", "https://github.com/meowfluff/JesusAI-Cartridges", "web_fpr", "list_2022_march", true, true),
     
     // february 2022
     new Tile("Make a SPECIL Valentine!", "A text-based dating sim about finding true love", "coding the entire game", "https://github.com/meowfluff/Make-a-SPECIL-Valentine", "web_fpr", "list_2022_february", true, true),
