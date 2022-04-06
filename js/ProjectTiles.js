f2c3babe51e438a315841e71ffc1cedc0e20186e
ritz <96433729+enfier@users.noreply.github.com>
2022-04-06 22:06:59 +0100

    Update ProjectTiles.js

--- js/ProjectTiles.js
+++ js/ProjectTiles.js
@@ -32,7 +32,7 @@ _PROJECTS = [
     new Tile("Auxilium", "A two player or singleplayer experience about teamwork and betrayal", "coding", "https://github.com/enfier/Auxilium", "web_fpr", "list_unreleased", true, true),
 
     // april 2022
-    new Tile("boot9strap", "Third-party CFW code executor", "establishing concept", "https://github.com/enfier/boot9strap", "web_fpr", "list_2022_april", "true", "true"),
+    new Tile("boot9strap", "Third-party CFW code executor", "establishing concept", "https://github.com/enfier/boot9strap", "web_fpr", "list_2022_april", true, true),
     
     // march 2022
     new Tile("JesusAI", "a batch game involving Jesus, debugging features and 4 endings", "co-developing, recoding parts of the game", "https://github.com/JesusAIexperience/JesusAI", "web_fpr", "list_2022_march", true, true),
